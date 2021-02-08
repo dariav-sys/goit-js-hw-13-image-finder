176fb385a6026626cca09cc713f7d4cb426635b8
Daria Viter <viter.daria@gmail.com>
2021-02-08 11:28:03 +0000

    commit

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,12 +1,18 @@
-import imgTemplate from "../templates/image-template.hbs";
+import imgTemplate from '../templates/image-template.hbs';
 
-import refs from "./refs";
+import refs from './refs';
 
 export default function markup(data) {
   // console.log(data.hits);
 
   return refs.gallery.insertAdjacentHTML(
-    "beforeend",
-    data.hits.map((image) => imgTemplate(image)).join("")
+    'beforeend',
+
+    data.hits
+      .map(image => {
+        console.log(data.hits);
+        return imgTemplate(image);
+      })
+      .join(''),
   );
 }
